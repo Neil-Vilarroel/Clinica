378ab963fd7d88c15b0917272bd0ebbafb510ee7
Neil <103211478+Neil-Vilarroel@users.noreply.github.com>
2022-09-19 15:49:49 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+require_once "Controladores/plantillaC.php";
+require_once "Controladores/secretariasC.php";
+require_once "Modelos/secretariasM.php";
+
+$plantilla = new Plantilla();
+$plantilla -> LlamarPlantilla();
